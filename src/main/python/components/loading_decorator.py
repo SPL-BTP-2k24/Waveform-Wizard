118b80d424aff06f8942492173ec7ca536f5a821
Abhinav Reddy Boddu <abhinavreddyboddu@gmail.com>
2024-11-02 11:11:08 +0530

    cleaned loading decorator

--- src/main/python/components/loading_decorator.py
+++ src/main/python/components/loading_decorator.py
@@ -2,13 +2,6 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtCore import Qt
 from functools import wraps
 
-class Overlay(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setAttribute(Qt.WA_TransparentForMouseEvents, False)  # Capture mouse events
-        self.setStyleSheet("background-color: rgba(0, 0, 0, 0.5);")  # Semi-transparent background
-        self.setWindowFlags(Qt.Window | Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
-
 def loading_decorator(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
